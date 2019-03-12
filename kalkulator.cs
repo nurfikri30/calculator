bfa09a4a2c591ea5680cfbfcf9f0382ed835d9c0
nurfikri <muhammadnurfikri286@gmail.com>
2019-03-12 11:54:06 +0700

    menampilkan variabel a

--- kalkulator.cs
+++ kalkulator.cs
@@ -2,3 +2,13 @@ using System;
 
 namespace CalculatorConsoleApp
 {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = 10 + 11;
+			Console.WriteLine(a);
+		}
+	}
+}
+//https://github.com/nurfikri30/calculator.git
